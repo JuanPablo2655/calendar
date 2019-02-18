83ba5025fc0e697d5f43386e96bd6472fd2b05de
Juan Pablo <34992574+JuanPablo2655@users.noreply.github.com>
2019-02-18 18:05:38 -0500

    messed with express and ejs

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.listen(port, () => console.log(`app is on port ${port}.`))

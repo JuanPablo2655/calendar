8292c2dccea45b487c31ae9d0f806f99ae42121c
Juan Pablo <34992574+JuanPablo2655@users.noreply.github.com>
2019-03-02 16:57:08 -0500

    months

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+let day = new Date();
+let month = day.getMonth();
+let year = day.getFullYear();
+
+let fullMonth = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let shortMonth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
